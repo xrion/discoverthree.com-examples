5d2794c7201baf1a448a84d3fc9957f9eb5b0dc4
Lewy Blue <looeee@gmail.com>
2018-03-25 15:24:10 +0700

    updated ch1

--- section-1/ch-1-getting-started/js/app.js
+++ section-1/ch-1-getting-started/js/app.js
@@ -1,74 +1,36 @@
+// create a Scene
+const scene = new THREE.Scene();
 
-// the camera and renderer are used in more than one function so we'll declare them first
-let camera;
-let renderer;
+// Create a Camera
+const fov = 35; // AKA "Field of View"
+const aspect = window.innerWidth / window.innerHeight;
+const near = 0.1; // the near clipping plane
+const far = 1000; // the far clipping plane
 
-// The init function will do all of the heavy lifting to create and animate the scene
-function init() {
+const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 
-  // Create the Scene
-  const scene = new THREE.Scene();
+// every object is initially created at ( 0, 0, 0 )
+// we'll move the camera back a bit so that we can view the scene
+camera.position.set( 0, 0, 40 );
 
-  // Create the Camera
-  const fov = 35; // AKA "Field of View"
-  const aspect = window.innerWidth / window.innerHeight;
-  const near = 0.1; // the near clipping plane
-  const far = 1000; // the far clipping plane
+// create a geometry
+const geometry = new THREE.TorusKnotBufferGeometry( 5, 1 );
 
-  camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+// create a default (white) Basic material
+const material = new THREE.MeshBasicMaterial();
 
-  camera.position.set( 0, 0, 40 );
+// create a Mesh containing the geometry and material
+const mesh = new THREE.Mesh( geometry, material );
 
-  // Set up camera controls
-  const controls = new THREE.OrbitControls( camera );
-  controls.enableDamping = true; // gives a feeling of "weight" to the controls
+// add the mesh to the scene
+scene.add( mesh );
 
-  // create a global illumination light
-  const ambientLight = new THREE.AmbientLight( 0xffffff, 1.0 );
-  scene.add( ambientLight );
+// set up the renderer
+const renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
 
-  // create an omnidirectional light
-  const pointLight = new THREE.PointLight( 0xffffff, 1.0 );
-  pointLight.position.set( 0, 0, 20 );
-  scene.add( pointLight );
+// add the automatically created <canvas> element to the page
+document.querySelector( '#container' ).appendChild( renderer.domElement );
 
-  // set up the renderer
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  // add the automatically created <canvas> element to the page
-  document.querySelector( '#container' ).appendChild( renderer.domElement );
-
-  // create a box
-  const boxGeometry = new THREE.BoxBufferGeometry( 15, 15, 15 );
-  const boxMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } );
-  const boxMesh = new THREE.Mesh( boxGeometry, boxMaterial );
-
-  // add the box to the scene
-  scene.add( boxMesh );
-
-  // set up a simple animation loop
-  renderer.animate( () => {
-    renderer.render( scene, camera );
-
-    // required of controls.enableDamping is set, see above
-    controls.update();
-  } );
-
-}
-
-// set up automatic resizing
-function onWindowResize() {
-
-  // reset the camera's aspect ratio to the new size
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-
-// add an event listener to the window which will fire when it changes size
-window.addEventListener( 'resize', onWindowResize );
-
-// call the init function to create the scene and start animating
-init();
+// 'render', or 'create a still image', of the scene
+renderer.render( scene, camera );
