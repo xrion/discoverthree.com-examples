d70c321c4ae06377b7238ca7462fa61841a5dc2a
Lewy Blue <looeee@gmail.com>
2018-03-25 14:41:52 +0700

    updated basic template, added eslint

--- basic-template/js/app.js
+++ basic-template/js/app.js
@@ -1,10 +1,12 @@
-let camera, renderer, scene;
+let camera;
+let renderer;
+let scene;
 
 function init() {
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setSize( window.innerWidth, window.innerHeight );
 
-  document.body.appendChild(renderer.domElement);
+  document.body.appendChild( renderer.domElement );
 
   scene = new THREE.Scene();
 
@@ -13,43 +15,43 @@ function init() {
   const near = 0.1; // the near clipping plane
   const far = 1000; // the far clipping plane
 
-  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 
-  camera.position.set(0, 0, 40);
+  camera.position.set( 0, 0, 40 );
 
-  const controls = new THREE.OrbitControls(camera);
+  const controls = new THREE.OrbitControls( camera );
 
   // create a global illumination light
-  const ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
-  scene.add(ambientLight);
+  const ambientLight = new THREE.AmbientLight( 0xffffff, 1.0 );
+  scene.add( ambientLight );
 
-  const pointLight = new THREE.PointLight(0xffffff, 1.0);
-  pointLight.position.set(0, 0, 20);
-  scene.add(pointLight);
+  const pointLight = new THREE.PointLight( 0xffffff, 1.0 );
+  pointLight.position.set( 0, 0, 20 );
+  scene.add( pointLight );
 }
 
 function initMeshes() {
-  const boxGeometry = new THREE.BoxBufferGeometry(15, 15, 15);
-  const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  const boxGeometry = new THREE.BoxBufferGeometry( 15, 15, 15 );
+  const boxMaterial = new THREE.MeshStandardMaterial( { color: 0xffffff } );
+  const boxMesh = new THREE.Mesh( boxGeometry, boxMaterial );
 
-  scene.add(boxMesh);
+  scene.add( boxMesh );
 }
 
 function animate() {
-  requestAnimationFrame(animate);
+  requestAnimationFrame( animate );
 
-  renderer.render(scene, camera);
+  renderer.render( scene, camera );
 }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-window.addEventListener("resize", onWindowResize);
+window.addEventListener( 'resize', onWindowResize );
 
 init();
 initMeshes();
