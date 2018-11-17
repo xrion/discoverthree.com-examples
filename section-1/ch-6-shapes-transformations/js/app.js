c7e239ef76b449d7f62d413a2f28e30f3b1b8492
Lewy Blue <looeee@gmail.com>
2018-11-17 13:02:36 +0530

    create train model

--- section-1/ch-6-shapes-transformations/js/app.js
+++ section-1/ch-6-shapes-transformations/js/app.js
@@ -36,7 +36,7 @@ function init() {
 function initCamera() {
 
   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
-  camera.position.set( 0, 0, 7 );
+  camera.position.set( 0, 0, 15 );
 
 }
 
@@ -70,6 +70,9 @@ function initMaterials() {
 
   return new THREE.MeshStandardMaterial( {
     map: texture,
+    flatShading: true,
+    roughness: 0.75,
+    metalness: 0.1,
   } );
 
 }
@@ -82,32 +85,39 @@ function initMeshes() {
 
   const material = initMaterials();
 
-  const bodyGeometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
-  const body = new THREE.Mesh( bodyGeometry, material );
-  body.scale.set( 1.5, 0.5, 0.5 );
-  car.add( body );
+  const noseGeometry = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 8 );
+  const nose = new THREE.Mesh( noseGeometry, material );
+  nose.rotation.set( Math.PI / 2, 0, Math.PI / 2 );
+  nose.position.x = -1.5;
 
-  const wheelGeo = new THREE.CylinderBufferGeometry( 0.4, 0.4, 0.15, 64 );
+  const cabinGeometry = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
+  const cabin = new THREE.Mesh( cabinGeometry, material );
+  cabin.position.set( 1, 0.4, 0 );
+
+  const chimneyGeometry = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
+  const chimney = new THREE.Mesh( chimneyGeometry, material );
+  chimney.position.set( -2.5, 0.9, 0 );
+
+  car.add( cabin, nose, chimney );
+
+  const wheelGeo = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
   wheelGeo.rotateX( Math.PI / 2 );
 
-  wheelFrontLeft = new THREE.Mesh( wheelGeo, material );
-  wheelFrontLeft.position.set( -1, -0.5, 0.6 );
-  car.add( wheelFrontLeft );
+  const smallWheelRear = new THREE.Mesh( wheelGeo, material );
+  smallWheelRear.position.set( -0.5, -0.5, 0 );
 
-  wheelFrontRight = new THREE.Mesh( wheelGeo, material );
-  wheelFrontRight.position.set( -1, -0.5, -0.6 );
-  wheelFrontRight.rotation.z = 2.5;
-  car.add( wheelFrontRight );
+  const smallWheelCenter = smallWheelRear.clone();
+  smallWheelCenter.position.x = -1.5;
 
-  wheelRearLeft = wheelFrontLeft.clone();
-  wheelRearLeft.position.x = 1;
-  wheelRearLeft.rotation.z = -2.5;
-  car.add( wheelRearLeft );
+  const smallWheelFront = smallWheelRear.clone();
+  smallWheelFront.position.x = -2.5;
 
-  wheelRearRight = wheelFrontRight.clone();
-  wheelRearRight.position.x = 1;
-  wheelRearRight.rotation.z = 1.75;
-  car.add( wheelRearRight );
+  const bigWheel = smallWheelRear.clone();
+  bigWheel.scale.set( 2, 2, 1.25 );
+  bigWheel.position.set( 1, -0.1, 0 );
+
+  car.add( smallWheelRear, smallWheelCenter, smallWheelFront, bigWheel );
+
 
 }
 
@@ -127,10 +137,10 @@ const rotationSpeed = 0.01;
 
 function update() {
 
-  wheelFrontLeft.rotation.z += rotationSpeed;
-  wheelFrontRight.rotation.z += rotationSpeed;
-  wheelRearLeft.rotation.z += rotationSpeed;
-  wheelRearRight.rotation.z += rotationSpeed;
+  // wheelFrontLeft.rotation.z += rotationSpeed;
+  // wheelFrontRight.rotation.z += rotationSpeed;
+  // wheelRearLeft.rotation.z += rotationSpeed;
+  // wheelRearRight.rotation.z += rotationSpeed;
 
 }
 
