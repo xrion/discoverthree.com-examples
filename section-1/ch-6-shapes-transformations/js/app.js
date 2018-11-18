6ccc1869b235c67794063d48df5068cc6d4090cf
Lewy Blue <looeee@gmail.com>
2018-11-18 11:18:57 +0530

    tweak

--- section-1/ch-6-shapes-transformations/js/app.js
+++ section-1/ch-6-shapes-transformations/js/app.js
@@ -30,7 +30,7 @@ function init() {
 function initCamera() {
 
   camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
-  camera.position.set( -5, 5, 10 );
+  camera.position.set( -5, 5, 7 );
 
 }
 
@@ -67,42 +67,44 @@ function initMeshes() {
   } );
 
   const detailMaterial = new THREE.MeshStandardMaterial( {
-    color: 0x333333, // dark grey
+    color: 0x333333, // darkgrey
     flatShading: true,
   } );
 
   const noseGeometry = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 12 );
   const nose = new THREE.Mesh( noseGeometry, bodyMaterial );
-  nose.rotation.z = Math.PI / 2;
-  nose.position.x = -1.5;
+  nose.rotation.set( Math.PI / 2, 0, Math.PI / 2 );
+  nose.position.x = -1;
 
   const cabinGeometry = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
   const cabin = new THREE.Mesh( cabinGeometry, bodyMaterial );
-  cabin.position.set( 1, 0.4, 0 );
+  cabin.position.set( 1.5, 0.4, 0 );
 
-  const chimneyGeometry = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
-  const chimney = new THREE.Mesh( chimneyGeometry, detailMaterial );
-  chimney.position.set( -2.5, 0.9, 0 );
-
-  train.add( cabin, nose, chimney );
+  train.add( cabin, nose );
 
   const wheelGeo = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
   wheelGeo.rotateX( Math.PI / 2 );
 
   const smallWheelRear = new THREE.Mesh( wheelGeo, detailMaterial );
-  smallWheelRear.position.set( -0.5, -0.5, 0 );
+  smallWheelRear.position.set( 0, -0.5, 0 );
 
   const smallWheelCenter = smallWheelRear.clone();
-  smallWheelCenter.position.x = -1.5;
+  smallWheelCenter.position.x = -1;
 
   const smallWheelFront = smallWheelRear.clone();
-  smallWheelFront.position.x = -2.5;
+  smallWheelFront.position.x = -2;
 
   const bigWheel = smallWheelRear.clone();
   bigWheel.scale.set( 2, 2, 1.25 );
-  bigWheel.position.set( 1, -0.1, 0 );
+  bigWheel.position.set( 1.5, -0.1, 0 );
 
   train.add( smallWheelRear, smallWheelCenter, smallWheelFront, bigWheel );
+
+  const chimneyGeometry = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
+  const chimney = new THREE.Mesh( chimneyGeometry, detailMaterial );
+  chimney.position.set( -2, 0.9, 0 );
+
+  train.add( chimney );
 
 }
 
