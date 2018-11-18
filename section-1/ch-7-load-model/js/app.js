02df9df3f9a29e7fbf2da43632a4e0bcc2f4f1af
Lewy Blue <looeee@gmail.com>
2018-11-18 15:21:51 +0530

    add bird models

--- section-1/ch-7-load-model/js/app.js
+++ section-1/ch-7-load-model/js/app.js
@@ -5,6 +5,9 @@ let controls;
 let renderer;
 let scene;
 
+let mixers = [];
+const clock = new THREE.Clock();
+
 function init() {
 
   container = document.querySelector( '#container' );
@@ -15,7 +18,7 @@ function init() {
   initCamera();
   initControls();
   initLights();
-  initMeshes();
+  loadModels();
   initRenderer();
 
   renderer.setAnimationLoop( () => {
@@ -29,8 +32,8 @@ function init() {
 
 function initCamera() {
 
-  camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 0.1, 100 );
-  camera.position.set( -5, 5, 7 );
+  camera = new THREE.PerspectiveCamera( 35, container.clientWidth / container.clientHeight, 1, 1000 );
+  camera.position.set( -50, 50, 150 );
 
 }
 
@@ -55,84 +58,35 @@ function initLights() {
 
 }
 
-function initMaterials() {
+function loadModels() {
 
-  const textureLoader = new THREE.TextureLoader();
+  const setupModel = ( gltf, position ) => {
 
-  // const diffuseMap = textureLoader.load( 'textures/uv_test.png' );
-  const diffuseMap = textureLoader.load( 'textures/greasy-pan-2-albedo.png' );
-  diffuseMap.anisotropy = 16;
+    const bird = gltf.scene;
+    bird.position.copy( position );
 
-  const normalMap = textureLoader.load( 'textures/greasy-pan-2-normal.png' );
-  const roughnessMap = textureLoader.load( 'textures/greasy-pan-2-roughness.png' );
-  const metalnessMap = textureLoader.load( 'textures/greasy-pan-2-metal.png' );
+    const animation = gltf.animations[ 0 ];
 
-  const materials = {};
+    const mixer = new THREE.AnimationMixer( bird );
+    mixers.push( mixer );
 
-  materials.body = new THREE.MeshStandardMaterial( {
-    color: 0x551111,
-    map: diffuseMap,
-    normalMap,
-    roughnessMap,
-    metalnessMap,
-    roughness: 1,
-    metalness: 1,
-  } );
+    const action = mixer.clipAction( animation );
+    action.play();
 
-  materials.detail = new THREE.MeshStandardMaterial( {
-    color: 0x333333,
-    // normalMap,
-    // roughnessMap,
-    roughness: 0.25,
-    metalness: 0,
-  } );
+    scene.add( bird );
 
-  return materials;
+  };
 
-}
+  const loader = new THREE.GLTFLoader();
 
-function initMeshes() {
+  const parrotPosition = new THREE.Vector3( 0, 0, 50 );
+  loader.load( 'models/parrot.glb', ( gltf ) => { setupModel( gltf, parrotPosition ); } );
 
-  // create a Group to hold the pieces of the train
-  const train = new THREE.Group();
-  scene.add( train );
+  const flamingoPosition = new THREE.Vector3( 150, 00, -200 );
+  loader.load( 'models/flamingo.glb', ( gltf ) => { setupModel( gltf, flamingoPosition ); } );
 
-  const materials = initMaterials();
-
-  const noseGeometry = new THREE.CylinderBufferGeometry( 0.75, 0.75, 3, 12 );
-  const nose = new THREE.Mesh( noseGeometry, materials.body );
-  nose.rotation.set( Math.PI / 2, 0, Math.PI / 2 );
-  nose.position.x = -1;
-
-  const cabinGeometry = new THREE.BoxBufferGeometry( 2, 2.25, 1.5 );
-  const cabin = new THREE.Mesh( cabinGeometry, materials.body );
-  cabin.position.set( 1.5, 0.4, 0 );
-
-  train.add( cabin, nose );
-
-  const wheelGeo = new THREE.CylinderBufferGeometry( 0.4, 0.4, 1.75, 16 );
-  wheelGeo.rotateX( Math.PI / 2 );
-
-  const smallWheelRear = new THREE.Mesh( wheelGeo, materials.detail );
-  smallWheelRear.position.set( 0, -0.5, 0 );
-
-  const smallWheelCenter = smallWheelRear.clone();
-  smallWheelCenter.position.x = -1;
-
-  const smallWheelFront = smallWheelRear.clone();
-  smallWheelFront.position.x = -2;
-
-  const bigWheel = smallWheelRear.clone();
-  bigWheel.scale.set( 2, 2, 1.25 );
-  bigWheel.position.set( 1.5, -0.1, 0 );
-
-  train.add( smallWheelRear, smallWheelCenter, smallWheelFront, bigWheel );
-
-  const chimneyGeometry = new THREE.CylinderBufferGeometry( 0.3, 0.1, 0.5 );
-  const chimney = new THREE.Mesh( chimneyGeometry, materials.detail );
-  chimney.position.set( -2, 0.9, 0 );
-
-  train.add( chimney );
+  const storkPosition = new THREE.Vector3( 0, -50, -200 );
+  loader.load( 'models/stork.glb', ( gltf ) => { setupModel( gltf, storkPosition ); } );
 
 }
 
@@ -149,7 +103,13 @@ function initRenderer() {
 
 }
 
-function update() {}
+function update() {
+
+  const delta = clock.getDelta();
+
+  mixers.forEach( ( mixer ) => { mixer.update( delta ) } );
+
+}
 
 function render() {
 
