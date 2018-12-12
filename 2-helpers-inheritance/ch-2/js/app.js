e191853e76edb1fdb9c493669e26d0d2b09158a9
Lewy Blue <looeee@gmail.com>
2018-12-12 09:39:43 +0800

    split App into seperate file

--- 2-helpers-inheritance/ch-2/js/app.js
+++ 2-helpers-inheritance/ch-2/js/app.js
@@ -16,7 +16,19 @@ class App {
 
     this.scene = new THREE.Scene();
     this.clock = new THREE.Clock();
+
     this.running = false;
+
+    // make sure to set these to the values you want before calling init
+    // since they can't be changed without creating a new WebGLRenderer
+    this.alpha = false;
+    this.antialias = true;
+
+    this.autoResize = true;
+
+  }
+
+  init() {
 
     this.initCamera();
     this.initControls();
@@ -35,14 +47,11 @@ class App {
 
   initControls() {
 
-    // Case 1: controls loaded via <script> tag
+    // if the controls script was loaded, we'll set them up
     if ( typeof THREE.OrbitControls === 'function' ) this.controls = new THREE.OrbitControls( this.camera, this.container );
-    // Case 2: controls loaded as ES6 module
-    else if ( typeof OrbitControls === 'function' ) this.controls = new OrbitControls( this.camera, this.container );
 
-    else {
-      console.error( 'Couldn\'t find the glTFLoader, please check that you have included the script correctly!' );
-    }
+    // otherwise we'll skip them
+    else return;
 
     // gives the controls a feeling of "weight"
     this.controls.enableDamping = true;
@@ -52,10 +61,6 @@ class App {
   initLoader() {
 
     if ( typeof THREE.GLTFLoader === 'function' ) this.loader = new THREE.GLTFLoader();
-    else if ( typeof GLTFLoader === 'function' ) this.loader = new GLTFLoader();
-    else {
-      console.error( 'Couldn\'t find the glTFLoader, please check that you have included the script correctly!' );
-    }
 
   }
 
@@ -63,8 +68,8 @@ class App {
 
     this.renderer = new THREE.WebGLRenderer( {
       powerPreference: 'high-performance',
-      alpha: true,
-      antialias: true,
+      alpha: this.alpha,
+      antialias: this.antialias,
     } );
 
     this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
@@ -126,6 +131,8 @@ class App {
 
   onWindowResize() {
 
+    if( ! this.autoResize ) return;
+
     this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
 
     this.camera.updateProjectionMatrix();
@@ -138,75 +145,3 @@ class App {
   }
 
 }
-
-const app = new App();
-
-app.scene.background = new THREE.Color( 0x8FBCD4 );
-app.camera.position.set( -50, 50, 150 );
-
-function initLights() {
-
-  const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
-  app.scene.add( ambientLight );
-
-  const frontLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  frontLight.position.set( 10, 10, 10 );
-
-  const backLight = new THREE.DirectionalLight( 0xffffff, 1 );
-  backLight.position.set( -10, 10, -10 );
-
-  app.scene.add( frontLight, backLight );
-
-}
-
-function loadModels() {
-
-  // A reusable function to setup the models
-  const onLoad = ( gltf, position ) => {
-
-    const model = gltf.scene.children[ 0 ];
-    model.position.copy( position );
-
-    const animation = gltf.animations[ 0 ];
-    const mixer = new THREE.AnimationMixer( model );
-
-    // we'll check every object in the scene for
-    // this function and call it once per frame
-    model.userData.onUpdate = ( delta ) => {
-
-      mixer.update( delta );
-
-    };
-
-    const action = mixer.clipAction( animation );
-    action.play();
-
-    app.scene.add( model );
-
-  };
-
-  const onError = ( errorMessage ) => { console.log( errorMessage ); };
-
-  // load the first model. Each model is loaded asynchronously,
-  // so don't make any assumption about which one will finish loading first
-  const parrotPosition = new THREE.Vector3( 0, 0, 50 );
-  app.loader.load( 'models/Parrot.glb', gltf => onLoad( gltf, parrotPosition ), null, onError );
-
-  const flamingoPosition = new THREE.Vector3( 150, 0, -200 );
-  app.loader.load( 'models/Flamingo.glb', gltf => onLoad( gltf, flamingoPosition ), null, onError );
-
-  const storkPosition = new THREE.Vector3( 0, -50, -200 );
-  app.loader.load( 'models/Stork.glb', gltf => onLoad( gltf, storkPosition ), null, onError );
-
-}
-
-initLights();
-loadModels();
-
-app.start();
-
-app.container.addEventListener( 'click', ( e ) => {
-
-  app.running ? app.stop() : app.start();
-
-} );
