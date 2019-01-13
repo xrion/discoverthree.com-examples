12fefe787dd5b3a0607b244c29bfcbd5eab560ff
Lewy Blue <looeee@gmail.com>
2019-01-13 19:26:08 +0800

    added 3.3a

---
+++ 3-cameras/3a-controls-orbit/js/main.js
@@ -0,0 +1,20 @@
+
+function init() {
+
+  const app = new THREE_APP( '#container' );
+
+  app.init();
+
+  app.scene.background = new THREE.Color( 0x8FBCD4 );
+  app.camera.position.set( -20, 30, 30 );
+
+  setupRenderer( app.renderer );
+  initLights( app.scene );
+  initMeshes( app.scene );
+  loadModels( app.scene, app.loader );
+
+
+  app.start();
+}
+
+init();
