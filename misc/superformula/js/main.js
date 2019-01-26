c13b579e517c05e0d897e340e7998c22f1a46f34
Lewy Blue <looeee@gmail.com>
2019-01-27 04:21:00 +0800

    added misc/superformula

---
+++ misc/superformula/js/main.js
@@ -0,0 +1,18 @@
+
+function init() {
+
+  const app = new THREE_APP( '#container' );
+
+  app.init();
+
+  app.scene.background = new THREE.Color( 0x8FBCD4 );
+  app.camera.position.set( 0, 0, 10 );
+
+  initLights( app.scene );
+  initMeshes( app.scene );
+
+  app.start();
+
+}
+
+init();
