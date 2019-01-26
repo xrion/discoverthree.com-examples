f1f40bba22ae8fafec86e442a23f4e06a12a772c
Lewy Blue <looeee@gmail.com>
2019-01-26 10:20:39 +0800

    updated 3.2c

--- 3-cameras/2c-orthographic-camera-2d-gui/js/lights.js
+++ 3-cameras/2c-orthographic-camera-2d-gui/js/lights.js
@@ -1,11 +1,11 @@
 function initLights( scene ) {
 
-  const ambientLight = new THREE.AmbientLight( 0xffffff, 1 );
+  const ambientLight = new THREE.HemisphereLight( 0xcccccc, 0x444444, 0.5 );
 
-  const frontLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  const frontLight = new THREE.DirectionalLight( 0xffffff, 0.75 );
   frontLight.position.set( 10, 10, 10 );
 
-  const backLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  const backLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
   backLight.position.set( -10, 10, -10 );
 
   scene.add( ambientLight, frontLight, backLight );
--- 3-cameras/2c-orthographic-camera-2d-gui/js/main.js
+++ 3-cameras/2c-orthographic-camera-2d-gui/js/main.js
@@ -4,7 +4,13 @@ function init() {
 
   app.init();
 
-  app.scene.background = new THREE.Color( 0x40a22f );
+  app.scene.background = new THREE.Color( 0x00BFFF );
+
+  // adding fog in the distance, the same color as the sky is
+  // a cheap way to blur the boundary between ground and sky
+  app.scene.fog = new THREE.Fog( 0x00BFFF, 115, 150 );
+
+
   app.camera.position.set( 10, 10, 20 );
 
   app.controls.target.y = 5;
@@ -16,6 +22,8 @@ function init() {
   const appGUI = initGUI();
 
   initLights( app.scene );
+
+  initGround( app.scene );
 
   loadModels( app.scene, app.loader, appGUI.scene );
 
