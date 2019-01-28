eb967ac1f18b03c50c058816cd32a1d90fcd9ec7
Lewy Blue <looeee@gmail.com>
2019-01-28 18:19:04 +0800

    added 6.4a

---
+++ 6-lights-shadows/4a-light-setup-stage/js/models.js
@@ -0,0 +1,34 @@
+const onLoad = ( gltf, scene ) => {
+
+
+  const model = gltf.scene.children[ 0 ];
+
+  console.log( 'model: ', model );
+
+  // todo: dummy positions not working
+  // const dummies = {
+
+  //   main: model.getObjectByName( 'main_light_dummy' ),
+  //   center: model.getObjectByName( 'stage_floor_center_dummy' ),
+
+  // };
+
+
+  initLights( scene );
+
+  scene.add( model );
+
+};
+
+function loadModels( scene ) {
+
+  const loader = new THREE.GLTFLoader();
+
+  THREE.DRACOLoader.setDecoderPath( 'js/vendor/draco/' );
+	loader.setDRACOLoader( new THREE.DRACOLoader() );
+
+  const onError = ( errorMessage ) => { console.log( errorMessage ); };
+
+  loader.load( 'models/stage.glb', gltf => onLoad( gltf, scene ), null, onError );
+
+}
