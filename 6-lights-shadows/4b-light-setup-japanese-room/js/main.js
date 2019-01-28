802b04a8127487a4ef8821a7030ab93fdd2f417c
Lewy Blue <looeee@gmail.com>
2019-01-28 21:09:50 +0800

    added 6.4b and c

---
+++ 6-lights-shadows/4b-light-setup-japanese-room/js/main.js
@@ -0,0 +1,21 @@
+
+function init() {
+
+  const app = new THREE_APP( '#container' );
+
+  app.init();
+
+
+  app.scene.background = new THREE.Color( 0x8FBCD4 );
+  app.camera.position.set( -10, 10, 10 );
+
+  setupRenderer( app.renderer );
+
+  initMeshes( app.scene );
+  loadModels( app.scene, app.loader );
+
+
+  app.start();
+}
+
+init();
---
+++ 6-lights-shadows/4b-light-setup-japanese-room/js/meshes.js
@@ -0,0 +1,4 @@
+function initMeshes( scene ) {
+
+}
+
