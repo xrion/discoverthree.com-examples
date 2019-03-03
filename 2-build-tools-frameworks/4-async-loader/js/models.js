bd8e6ad98ba9dfe37f10455018ec7d591463d80e
Lewy Blue <looeee@gmail.com>
2019-03-03 17:24:33 +0800

    improve async code

--- 2-build-tools-frameworks/4-async-loader/js/models.js
+++ 2-build-tools-frameworks/4-async-loader/js/models.js
@@ -2,26 +2,31 @@ import createAsyncLoader from './utility/createAsyncLoader.js';
 
 import { GLTFLoader } from './vendor/three/loaders/GLTFLoader.js';
 
-export default async function loadGLTFModels() {
+export default async function loadModels() {
 
   const loader = createAsyncLoader( new GLTFLoader() );
 
-  let gltf = await loader.load( 'models/Parrot.glb' );
+  // first, start all the async operation
+  const parrotPromise = loader.load( 'models/Parrot.glb' );
+  const flamingoPromise = loader.load( 'models/Flamingo.glb' );
+  const storkPromise = loader.load( 'models/Stork.glb' );
 
-  const parrot = gltf.scene.children[ 0 ];
-  parrot.animations = gltf.animations;
+  // next, wait for them to complete
+  const parrotResult = await parrotPromise;
+  const flamingoResult = await flamingoPromise;
+  const storkResult = await storkPromise;
+
+  // finally, set up the models
+  const parrot = parrotResult.scene.children[ 0 ];
+  parrot.animations = parrotResult.animations;
   parrot.position.set( 0, 0, 2.5 );
 
-  gltf = await loader.load( 'models/Flamingo.glb' );
-
-  const flamingo = gltf.scene.children[ 0 ];
-  flamingo.animations = gltf.animations;
+  const flamingo = flamingoResult.scene.children[ 0 ];
+  flamingo.animations = flamingoResult.animations;
   flamingo.position.set( 7.5, 0, -10 );
 
-  gltf = await loader.load( 'models/Stork.glb' );
-
-  const stork = gltf.scene.children[ 0 ];
-  stork.animations = gltf.animations;
+  const stork = storkResult.scene.children[ 0 ];
+  stork.animations = storkResult.animations;
   stork.position.set( 0, -2.5, -10 );
 
   return {
