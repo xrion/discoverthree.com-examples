3dc0d56ec062a4b608e7a116b16d59c2ef58cc04
Lewy Blue <looeee@gmail.com>
2019-07-18 12:36:14 +0700

    moved examples to draft

--- bulletproof-app/bulletproof-structure/js/BirdsScene/lighting/createAmbientLight.js
+++
@@ -1,9 +0,0 @@
-import {
-  HemisphereLight,
-} from '../vendor/three/three.module.js';
-
-export default function createAmbientLight() {
-
-  return new HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
-
-}
